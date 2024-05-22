352333c85cc2018955b6061438d09c79e5a8cd8c
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2024-05-22 22:55:16 +0300

    add sample data, add custom fonts

---
+++ app/design/frontend/Vendor/tasks3_theme/registration.php
@@ -0,0 +1,6 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Vendor/tasks3_theme', __DIR__);
+
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,14 @@
+module.exports = {
+    themes: {
+        your_theme: {
+            area: 'frontend',
+            name: 'Vendor/tasks3_theme',
+            locale: 'en_US',
+            files: [
+                'css/styles-m',
+                'css/styles-l'
+            ],
+            dsl: 'less'
+        }
+    }
+};
