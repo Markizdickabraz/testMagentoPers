62da88ef416ae65667a542ad4e79e89f1beaf814
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2024-05-23 19:24:09 +0300

    title margin top mobile done

--- app/design/frontend/Vendor/tasks3_theme/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Vendor/tasks3_theme', __DIR__);
-
--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -2,7 +2,7 @@ module.exports = {
     themes: {
         your_theme: {
             area: 'frontend',
-            name: 'Vendor/tasks3_theme',
+            name: 'Vendor/tasks6_theme',
             locale: 'en_US',
             files: [
                 'css/styles-m',
